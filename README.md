aff5568cfb47e1c2863b950d4e00b87c9146f149
zsxdb <Sx82199609124535@outlook.com>
2020-11-13 00:47:38 +0800

    update config

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # first-demo
 
-## Project setup
+## 项目包含了vue3.0的路由设置及语法演示
+
+* 指令的封装和使用
+* 路由设置
+* vue.config.js的配置
+
+### Project setup
 ```
 npm install
 ```
@@ -24,8 +30,3 @@ npm run lint
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
 ***
-
-### 项目包含了vue3.0的路由设置及语法演示
-
-* 指令的封装和使用
-* 路由设置
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,29 @@
 const path = require('path')
 
+// path.resolve添加子路径
+const resolve = dir => path.resolve(dir)
+
+// 端口号
+const port = 9550
+
 module.exports = {
     // 打包出的文件名，默认为dist
     outputDir: 'dist',
+    // 支持所有 webpack-dev-server 的选项
+    devServer: {
+        port: port,
+        // open当项目启动完成
+        open: true,
+        // proxy: 'http://localhost:8082/'
+    },
     // 可使用webpack配置
     configureWebpack: {
         resolve: {
+            // 路径映射
             alias: {
-                '@Component': path.resolve('src/components'),
-                '@Directive': path.resolve('src/directive')
+                '@Component': resolve('src/components'),
+                '@Directive': resolve('src/directive'),
+                '@Util': resolve('src/util')
             }
         }
     }
